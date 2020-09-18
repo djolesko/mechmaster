773765053945516c88a80fcd89448b355795d69e
djolesko <67395792+djolesko@users.noreply.github.com>
2020-09-18 16:48:52 +0200

    add 1 frame for db lookup

--- get_db.py
+++ get_db.py
@@ -4,7 +4,12 @@ import sqlite3
 
 conn=sqlite3.connect('mechmaster.db')
 c= conn.cursor()
-c.execute(""" INSERT INTO mechmaster_test(ime,prezime,datum,opis) VALUES(?,?,?,?)
-""",('djordje','papaz','05-07-1988','mali servis'))
+c.execute(""" INSERT INTO mechmaster_test(iznos,faktura,kilometraza,napomene,model) VALUES(?,?,?,?,?)
+""",('5000','255/20','135000','viljuska otisla','corsa'))
+#conn.commit()
+#conn.close()
+
+#c.execute("""ALTER TABLE mechmaster_test
+#	add column model text""")
 conn.commit()
 conn.close()
--- mechmaster.py
+++ mechmaster.py
@@ -12,89 +12,102 @@ SMALL_FONT = ("OpenSans", 10)
 
 class Mechmaster(tk.Tk):
 
-	def __init__(self, *args, **kwargs):
-		
-		tk.Tk.__init__(self, *args, **kwargs)
-		container = tk.Frame(self)
+    def __init__(self, *args, **kwargs):
+        # tk.Tk.iconbitmap(self,"icon.ico")
 
-		container.grid()
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        container.pack(side="top",fill="both", expand=True)
 
-		container.grid_rowconfigure(0, weight=1)
-		container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        #tk.Tk.iconbitmap(self, "containertent_portal_Hm6_icon.ico")
+        self.frames = {}
+        self.wm_title('Mechmaster 0.9')
 
-		self.frames = {}
+        for F in (StartPage, PageOne, PageTwo):
+            frame = F(container, self)
+            self.frames[F] = frame
+            # self.frames[PageOne] = frame
 
-		for F in (StartPage, PageOne, PageTwo):
+            frame.grid(row=0, column=0, sticky="nsew")
 
-			frame = F(container, self)
+        self.show_frame(StartPage)
 
-			self.frames[F] = frame
-
-			frame.grid(row=0, column=0, sticky="nsew")
-
-		self.show_frame(StartPage)
-
-	def show_frame(self, cont):
-
-		frame = self.frames[cont]
-		frame.tkraise()
-
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 		
 class StartPage(tk.Frame):
 
-		def __init__(self, parent, controller):
-				tk.Frame.__init__(self, parent)
-				header_label=tk.Label(text='Dobrodosli u Mechmaster',font=LARGE_FONT,bg='gray31', fg='white').grid(column=5,row=0)
-				header2_label=tk.Label(text='Izaberite opcije',font=NORMAL_FONT,bg='gray31', fg='white').grid(column=5,row=1)
-				button1 = tk.Button(text='Lista Kupaca', bg='gray35', fg='white', font=NORMAL_FONT,command=lambda : tree()).grid()
-				button2 = tk.Button(text='Novi Kupac', bg='gray35', fg='white', font=NORMAL_FONT).grid()
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg='gray31')
+        label = tk.Label(self, text="MECH MASTER PRO ", bg='gray31', fg='white', font=LARGE_FONT, height=3, width=30)
+        label.pack(pady=20, padx=20)
+
+        button1 = tk.Button(self, text="Otvori Bazu", bg='medium aquamarine', fg='white', width=40,
+                            height=4, font=NORMAL_FONT, command=lambda: controller.show_frame(PageOne))
+        button1.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text="Novi Kupac", bg='orange', fg='white', width=40, height=4, font=NORMAL_FONT,
+                            command=lambda: controller.show_frame(PageTwo))
+        button1.pack(pady=10, padx=10)
 
 
 class PageOne(tk.Frame):
 
-		def __init__(self, parent, controller):
-			tk.Frame.__init__(self, parent,bg='gray31')
-			
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent,bg='gray31')
+		button1  = tk.Button(self,text="Ucitaj postojece", font = NORMAL_FONT, command=lambda : import_data()).pack()
+		button1  = tk.Button(self,text="Nazad", font = NORMAL_FONT, command=lambda : [refresh(),controller.show_frame(StartPage)]).pack()
+		def import_data():
+			conn=sqlite3.connect('mechmaster.db')
+			df = pd.read_sql("select * from mechmaster_test",con=conn)
 			global tree
-			def tree():
-					conn=sqlite3.connect('mechmaster.db')
-					df = pd.read_sql("select * from mechmaster_test",con=conn)
-					tree = ttk.Treeview(columns=("ime", "prezime", "datum", "opis"),show='headings')
-					for i in tree.get_children():
-							tree.delete(i)
-					tree.heading('#1', text='ime')
-					tree.heading('#2', text='prezime')
-					tree.heading('#3', text='datum')
-					tree.heading('#4', text='opis')
-					tree.column('#1',stretch=NO)
-					tree.column('#2',stretch=NO)
-					tree.column('#3',stretch=NO)
-					tree.column('#4',stretch=NO)
-					for column, row in df.iterrows():		
-							tree.insert('', END, values=list(row))
-							tree.columnconfigure(0,weight=1)
-							tree.place(anchor='nw', relheight=0.4, relwidth=1.0, rely=0.6)
-				#tree.bind('<ButtonRelease-1>', on_double_click)
+			tree = ttk.Treeview(columns=("ime", "prezime", "datum", "opis","iznos","faktura","kilometraza","napmomene","model"),show='headings')
+			for i in tree.get_children():
+					tree.delete(i)
+			tree.heading('#1', text='ime')
+			tree.heading('#2', text='prezime')
+			tree.heading('#3', text='datum')
+			tree.heading('#4', text='opis')
+			tree.heading('#5', text='iznos')
+			tree.heading('#6', text='faktura')
+			tree.heading('#7', text='kilometraza')
+			tree.heading('#8', text='napomene')
+			tree.heading('#9', text='model')
+			tree.column('#1',width = 80,stretch=NO)
+			tree.column('#2',width = 80,stretch=NO)
+			tree.column('#3',width = 80,stretch=NO)
+			tree.column('#4',width = 340,stretch=NO)
+			tree.column('#5',width = 60,stretch=NO)
+			tree.column('#6',width = 60,stretch=NO)
+			tree.column('#7',width = 80,stretch=NO)
+			tree.column('#8',width = 340,stretch=NO)
+			tree.column('#9',stretch=NO)
+			for column, row in df.iterrows():		
+					tree.insert('', END, values=list(row))
+					tree.columnconfigure(0,weight=1)
+					tree.place(anchor='nw', relheight=0.4, relwidth=1.0, rely=0.6)
+			#tree.bind('<ButtonRelease-1>', on_double_click)
+		
+		def refresh():
+			tree.destroy()
+			
+			
+
 		
 class PageTwo(tk.Frame):
 
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-		label.grid()
-
-		button1 = tk.Button(self, text="Back to Home",
-							command=lambda: controller.show_frame(StartPage))
-		button1.grid()
-
-		button2 = tk.Button(self, text="Page One",
-							command=lambda: controller.show_frame(PageOne))
-		button2.grid()
-		
+		#self.grid_rowconfigure(5, minsize=2000, weight=1)
+		header_label2=tk.Label(self,text='Dobrodosli u Mechmaster page two',font=LARGE_FONT,bg='gray31', fg='white').pack()
+		#e0 = Text(self, height=3, width=30, wrap="none").grid()
 
 
 
 app = Mechmaster()
-app.geometry("")
+app.geometry('1280x720')
 app.mainloop()
 
